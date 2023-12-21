a59cc7caf7f5aeed22fa5f71f6ea2e373eb60c51
Ravi Tokas <ravitokas90@gmail.com>
2023-12-21 13:43:52 +0530

    add mint function

--- SuperToken.sol
+++ SuperToken.sol
@@ -4,4 +4,8 @@ pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract SuperToken {}
+contract SuperToken is ERC20 {
+    constructor() ERC20("SUPERTOKE", "SUP") {
+        _mint(msg.sender, 100);
+    }
+}
